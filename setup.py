74fc6bc77d1279a2bd429b292b8aed3b59dd0c7b
Zerocopia <zerodevsystem@gmail.com>
2024-10-11 19:39:16 -0300

    chore: criação do arquivo setup.py para configuração e empacotamento do projeto

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='pbcda',
+    packages=find_packages(),
+    version='0.1.0',
+    description='TP 2',
+    author='Fábio Linhares',
+    license='GNU General Public License (GPL) v3',
+)
