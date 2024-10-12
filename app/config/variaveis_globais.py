e036fd0e97431cbde2c8116d6f519556a13da18e
Zerocopia <zerodevsystem@gmail.com>
2024-10-12 00:14:20 -0300

    Update

---
+++ app/config/variaveis_globais.py
@@ -0,0 +1,14 @@
+import os
+
+streamlit_secret = os.path.join(os.path.dirname(__file__), '../../.streamlit/secrets.toml')
+
+image_directory = os.path.join(os.path.dirname(__file__), '../../images/.backgound')
+infnet_image = os.path.join(os.path.dirname(__file__), '../../images/infnet30.png')
+mec_image = os.path.join(os.path.dirname(__file__), '../../images/mec.png')
+
+
+arquivo_de_apresentacao = os.path.join(os.path.dirname(__file__), '../../data/interim/apresentacao.txt')
+arquivo_de_teste = os.path.join(os.path.dirname(__file__), '../../data/interim/teste.txt')
+arquivo_de_rubrica = os.path.join(os.path.dirname(__file__), '../../data/interim/rubricas.txt')
+
+API_BASE_URL = "http://localhost:8000"
