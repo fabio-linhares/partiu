26c6d2cf9b205ef8bab54a0683c4654a5803bed0
Zerocopia <zerodevsystem@gmail.com>
2024-10-12 17:09:50 -0300

    Add new file paths and update API base URL in variaveis_globais.py

--- app/config/variaveis_globais.py
+++ app/config/variaveis_globais.py
@@ -10,5 +10,13 @@ mec_image = os.path.join(os.path.dirname(__file__), '../../images/mec.png')
 arquivo_de_apresentacao = os.path.join(os.path.dirname(__file__), '../../data/interim/apresentacao.txt')
 arquivo_de_teste = os.path.join(os.path.dirname(__file__), '../../data/interim/teste.txt')
 arquivo_de_rubrica = os.path.join(os.path.dirname(__file__), '../../data/interim/rubricas.txt')
+arquivo_de_palavras = os.path.join(os.path.dirname(__file__), '../../data/external/dados_completos.json')
+
+
+# respostas
+arquivo_de_resposta1 = os.path.join(os.path.dirname(__file__), '../../data/interim/answers/resposta1.txt')
+arquivo_de_resposta2 = os.path.join(os.path.dirname(__file__), '../../data/interim/answers/resposta2.txt')
+arquivo_de_resposta3 = os.path.join(os.path.dirname(__file__), '../../data/interim/answers/resposta3.txt')
 
 API_BASE_URL = "http://179.124.242.238:8000"
+
