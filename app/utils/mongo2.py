836e4f3a7b8cbc3ca3081e5272afdc3ef0ae236d
Zerocopia <zerodevsystem@gmail.com>
2024-10-12 00:17:11 -0300

    feat: recurso para exibir a uri de conexão com o banco.

---
+++ app/utils/mongo2.py
@@ -0,0 +1,8 @@
+import toml
+
+def load_database_config(file_path):
+    config = toml.load(file_path)
+    db_access = config['database_access']
+    uri = f"{db_access['protocol']}://{db_access['user']}:{db_access['password']}@{db_access['host']}:{db_access['port']}/{db_access['database']}"
+    return uri
+
