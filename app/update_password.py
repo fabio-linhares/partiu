deb01f96a5ef9c8391ae858a221efedfc947a61f
Zerocopia <zerodevsystem@gmail.com>
2024-10-12 21:14:15 -0300

    Refactor update_password.py to add password update functionality

---
+++ app/update_password.py
@@ -0,0 +1,27 @@
+from pymongo import MongoClient
+from werkzeug.security import generate_password_hash
+from config.variaveis_globais import streamlit_secret
+from utils.globals import create_global_variables
+from utils.database import get_connection_string
+
+config = create_global_variables(streamlit_secret)
+
+def update_password_hash(db, username, password):
+    users_collection = db[config['collections_users']]
+    hashed_password = generate_password_hash(password)
+    result = users_collection.update_one({"username": username}, {"$set": {"password": hashed_password}})
+    return result.modified_count > 0
+
+if __name__ == "__main__":
+    client = MongoClient(get_connection_string())
+    db = client[config['database_user']]
+
+    username = input("Digite o nome de usuário: ")
+    password = input("Digite a nova senha: ")
+
+    if update_password_hash(db, username, password):
+        print(f"Senha atualizada com sucesso para o usuário {username}")
+    else:
+        print(f"Não foi possível atualizar a senha para o usuário {username}")
+
+    client.close()
