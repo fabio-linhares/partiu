6c8ec7dc92786926fd31a08c1ffe738f2fd5503a
Zerocopia <zerodevsystem@gmail.com>
2024-10-18 15:31:53 -0300

    Refactor render.py to remove unused imports and update code structure

--- app/utils/render.py
+++ app/utils/render.py
@@ -1,64 +1,28 @@
-# Built-in libraries
-import json
-import threading
-import time
-from datetime import datetime, timedelta
-
 # Third-party libraries
-import matplotlib.pyplot as plt
 import streamlit as st
 
-import requests
-from PIL import Image
-import io
 
 # Local modules
-from api import api_request, get_sections_from_api, api_request_cached
 from config.variaveis_globais import (
     streamlit_secret, 
-    image_directory, 
-    infnet_image, 
-    mec_image, 
+    image_directory,  
     arquivo_de_apresentacao, 
     arquivo_de_teste, 
-    arquivo_de_rubrica,
-    arquivo_de_resposta1,
-    arquivo_de_resposta2,
-    arquivo_de_resposta3,
-    arquivo_de_resposta4,
-    arquivo_de_palavras
+    arquivo_de_rubrica
 )
 
 
-
-from utils.abas import (criar_abas_principais, 
-                        criar_abas_secundarias, 
-                        adicionar_conteudo,
-                        criar_exibidor_respostas,
+from utils.abas import (adicionar_conteudo,
                         exibir_respostas,
                         exibir_pacotes_viagem,
                         exibir_area_restrita,
                         exibir_api_teste,)
 
 from utils.markdown import read_markdown_file
-from utils.background import get_random_image, get_cached_random_image
-from utils.cadastro import render_cadastro_form
+from utils.background import get_cached_random_image
 from utils.globals import create_global_variables
 
-from utils.database import get_user_data
-from utils.loadfile import load_json_data, save_uploaded_file
-from utils.mail import enviar_email
-from utils.markdown import read_markdown_file
-from utils.mongo2 import load_database_config
-from utils.scrapy  import get_pacotes_viagem, atualizar_pacotes
-from utils.scraper import run_scraper
-from utils.security import login_user
-from utils.title import get_random_title
-from utils.frescuras import (gerar_nuvem_palavras,
-                             contar_itens_config,
-                             get_tab_names,
-                             exibir_grafico_precos,
-                             exibir_tabela_ofertas)
+from utils.frescuras import (contar_itens_config)
 
 
 
@@ -66,41 +30,6 @@ from utils.frescuras import (gerar_nuvem_palavras,
 ############################       SECRETS.TOML       ###########################
 #################################################################################
 config_vars = create_global_variables(streamlit_secret)
-
-if config_vars.get('environment_env') == 'dev':
-    print("Variáveis carregadas do arquivo TOML:")
-    for var_name, value in config_vars.items():
-        if var_name != 'environment_env':
-            print(f"{var_name} = {value}")
-
-    uri = load_database_config(streamlit_secret)
-    print("URI do banco de dados:", uri)
-
-
-#################################################################################
-############################         VARIÁVEIS        ###########################
-#################################################################################
-
-dev_data = get_user_data(database_name=config_vars['database_user'], 
-                                     collection_name=config_vars['collections_dev'])
-
-menu_dados = get_sections_from_api(config_vars['database_main'], 
-                                   config_vars['collections_menu'])
-
-num_main_tabs = contar_itens_config(config_vars, 'main_tab')
-num_sub_tabs = contar_itens_config(config_vars, 'sub_tab')
-num_sub_tabs_test = contar_itens_config(config_vars, 'sub_tab_test')
-
-
-
-
-
-
-
-
-
-
-
 
 
 def render_main_image():
